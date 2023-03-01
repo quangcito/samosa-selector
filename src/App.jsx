3a456331ec736383bd2435cd45692fdd9151468e
Quang Nguyen <quanguyendg@gmail.com>
2023-02-28 21:21:47 -0600

    add upgrades

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,23 @@ const App = () => {
         <h2>Count: {count}</h2>
         <img className="samosa" src="src/assets/samosa.png" onClick={updateCount} />
       </div>
+      <div className="container">
+        <div className="upgrade">
+          <h3>Double Stuffed</h3>
+          <p>2x per click</p>
+          <button>10 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Party Pack</h3>
+          <p>5x per click</p>
+          <button>100 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Full Feast</h3>
+          <p>10x per click</p>
+          <button>1000 samosas</button>
+        </div>
+      </div>
     </div>
   );
 }
