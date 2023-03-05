ae3580e1bc8fc0d15b6ba0c484dd56113c0cee64
Quang Nguyen <quanguyendg@gmail.com>
2023-03-05 01:36:55 -0600

    finish samosa selector

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,27 @@ const App = () => {
 
   const updateCount = () => setCount(count + multiplier);
 
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);
+      setCount(count - 10);
+    }
+  }
+
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+      setCount(count - 100);
+    }
+  }
+
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setMultiplier(multiplier * 10);
+      setCount(count - 1000);
+    }
+  }
+
   return (
     <div className="App">
       <div className="header">
@@ -18,17 +39,17 @@ const App = () => {
         <div className="upgrade">
           <h3>Double Stuffed</h3>
           <p>2x per click</p>
-          <button>10 samosas</button>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
         </div>
         <div className="upgrade">
           <h3>Party Pack</h3>
           <p>5x per click</p>
-          <button>100 samosas</button>
+          <button onClick={buyPartyPack}>100 samosas</button>
         </div>
         <div className="upgrade">
           <h3>Full Feast</h3>
           <p>10x per click</p>
-          <button>1000 samosas</button>
+          <button onClick={buyFullFeast}>1000 samosas</button>
         </div>
       </div>
     </div>
